6c9b47c1d1eaab629f9ad63de7270050fb0c8702
Juan Etxenike Almeida <xaresd@gmail.com>
2022-08-15 19:22:06 +0200

    New html files for different bulma components

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
-const HtmlWebpackPlugin =  require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  mode : 'development',
-  entry : './js/app.js',
-  devServer: {
-    liveReload: true,
-    hot: true,
-    open: true,
-    static: ['./'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    })
-  ]
+    mode: 'development',
+    entry: './js/app.js',
+    devServer: {
+        liveReload: true,
+        hot: true,
+        open: true,
+        static: ['./'],
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader'
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                        // options...
+                    }
+                }
+            ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/style.css'
+        }),
+    ]
 };
